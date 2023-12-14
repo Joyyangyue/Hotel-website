b595a5bdda95e17bba4c13f902ebe07783617bf3
markoo37 <spitz.marko@gmail.com>
2023-12-14 13:59:30 +0100

    final

--- Hotel Website/hotel_website.sql
+++ Hotel Website/hotel_website.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Gép: 127.0.0.1
--- Létrehozás ideje: 2023. Dec 07. 16:12
--- Kiszolgáló verziója: 10.4.32-MariaDB
--- PHP verzió: 8.2.12
+-- Host: 127.0.0.1
+-- Generation Time: Dec 14, 2023 at 01:58 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,26 +18,42 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Adatbázis: `hotel_website`
+-- Database: `hotel_website`
 --
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `bookings`
+-- Table structure for table `bookings`
 --
 
 CREATE TABLE `bookings` (
   `user_id` int(11) NOT NULL,
   `hotel_id` int(11) NOT NULL,
   `arrival` date NOT NULL,
-  `leaving` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `leaving` date NOT NULL,
+  `guest_number` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bookings`
+--
+
+INSERT INTO `bookings` (`user_id`, `hotel_id`, `arrival`, `leaving`, `guest_number`) VALUES
+(2, 1, '2023-12-09', '2023-12-23', 0),
+(2, 1, '2023-12-29', '2023-12-30', 0),
+(2, 1, '2023-12-21', '2023-12-29', 0),
+(2, 1, '2023-12-23', '2023-12-29', 1),
+(2, 1, '2023-12-22', '2023-12-29', 1),
+(2, 1, '2023-12-22', '2023-12-30', 1),
+(2, 1, '2023-12-15', '2023-12-28', 1),
+(2, 1, '2023-12-15', '2023-12-24', 1),
+(2, 5, '2023-12-21', '2023-12-29', 4);
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `contact_messages`
+-- Table structure for table `contact_messages`
 --
 
 CREATE TABLE `contact_messages` (
@@ -47,12 +63,12 @@ CREATE TABLE `contact_messages` (
   `number` varchar(255) NOT NULL,
   `subject` varchar(255) NOT NULL,
   `message` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `hotel`
+-- Table structure for table `hotel`
 --
 
 CREATE TABLE `hotel` (
@@ -60,20 +76,26 @@ CREATE TABLE `hotel` (
   `destination_name` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
   `price` int(11) NOT NULL,
-  `available_space` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `available_space` int(11) NOT NULL,
+  `image_path` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- A tábla adatainak kiíratása `hotel`
+-- Dumping data for table `hotel`
 --
 
-INSERT INTO `hotel` (`id`, `destination_name`, `description`, `price`, `available_space`) VALUES
-(1, 'Budapest', 'Welcome to Budapest: The Majesty of the Danube and Cultural Richness.', 94, 5);
+INSERT INTO `hotel` (`id`, `destination_name`, `description`, `price`, `available_space`, `image_path`) VALUES
+(1, 'Budapest', 'Welcome to Budapest: The Majesty of the Danube and Cultural Richness.', 94, 24, 'p-1.jpg'),
+(2, 'Italy', 'Italy is a beautiful country in Southern Europe, known for its rich history, culture and excellent gastronomy.', 100, 13, 'p-2.jpg'),
+(3, 'London', 'It is a city that combines rich history, modernity and cultural diversity.', 179, 2, 'p-3.jpg'),
+(4, 'turkey', 'Turkey is a unique country that combines a rich history,and offers visitors incredible opportunities for travel and discovery.', 68, 0, 'p-4.jpeg'),
+(5, 'morocco', 'Morocco is a place where historical and cultural influences mix, including Arab, Berber and European influences.', 77, 41, 'p-5.jpg'),
+(6, 'laos', 'Laos is a place where influences from different cultures meet, including Hinduism, Buddhism and French colonial heritage.', 85, 5, 'p-6.png');
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -82,73 +104,74 @@ CREATE TABLE `user` (
   `password` varchar(255) NOT NULL,
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- A tábla adatainak kiíratása `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`ID`, `email`, `password`, `first_name`, `last_name`) VALUES
 (2, 's@gmail.com', '7c6a180b36896a0a8c02787eeafb0e4c', 'bla', 'bla'),
-(7, 'a@gmail.com', '482c811da5d5b4bc6d497ffa98491e38', 'a', 'a');
+(7, 'a@gmail.com', '482c811da5d5b4bc6d497ffa98491e38', 'a', 'a'),
+(8, 'm@gmail.com', '482c811da5d5b4bc6d497ffa98491e38', 'Mark', 'Marl');
 
 --
--- Indexek a kiírt táblákhoz
+-- Indexes for dumped tables
 --
 
 --
--- A tábla indexei `bookings`
+-- Indexes for table `bookings`
 --
 ALTER TABLE `bookings`
   ADD KEY `user_connection` (`user_id`),
   ADD KEY `hotel_connection` (`hotel_id`);
 
 --
--- A tábla indexei `contact_messages`
+-- Indexes for table `contact_messages`
 --
 ALTER TABLE `contact_messages`
   ADD PRIMARY KEY (`id`);
 
 --
--- A tábla indexei `hotel`
+-- Indexes for table `hotel`
 --
 ALTER TABLE `hotel`
   ADD PRIMARY KEY (`id`);
 
 --
--- A tábla indexei `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`ID`);
 
 --
--- A kiírt táblák AUTO_INCREMENT értéke
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT a táblához `contact_messages`
+-- AUTO_INCREMENT for table `contact_messages`
 --
 ALTER TABLE `contact_messages`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT a táblához `hotel`
+-- AUTO_INCREMENT for table `hotel`
 --
 ALTER TABLE `hotel`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT a táblához `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- Megkötések a kiírt táblákhoz
+-- Constraints for dumped tables
 --
 
 --
--- Megkötések a táblához `bookings`
+-- Constraints for table `bookings`
 --
 ALTER TABLE `bookings`
   ADD CONSTRAINT `hotel_connection` FOREIGN KEY (`hotel_id`) REFERENCES `hotel` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
